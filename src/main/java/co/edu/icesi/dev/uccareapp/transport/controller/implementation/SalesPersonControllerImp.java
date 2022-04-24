8fff217c687fa4a57f19bbeb8a8d02c006ce7717
YoNoSoySantiago <santiago.hurtado1@hormail.com>
2022-04-23 23:02:19 -0500

    all templates finished

---
+++ src/main/java/co/edu/icesi/dev/uccareapp/transport/controller/implementation/SalesPersonControllerImp.java
@@ -0,0 +1,8 @@
+package co.edu.icesi.dev.uccareapp.transport.controller.implementation;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SalesPersonControllerImp {
+
+}
